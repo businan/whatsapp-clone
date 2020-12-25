62e39a72e2b1145abf8a5c550c0e455b9b3df49b
Kevser Muslu <kevsermuslu@gmail.com>
2020-12-25 09:36:39 -0800

    H2

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -18,6 +18,13 @@ app.use(express.json());
 app.use(cors());
 app.use("/", router);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("whatsapp-clone/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "whatsapp-clone", "build", "index.html"))
+    })
+  }
+
 app.listen(port, () => console.log(`Listening on localhost: ${port}`))
 
 
